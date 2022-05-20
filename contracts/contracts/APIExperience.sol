08193a36a4e7039d690815ea958bbc37f3fa1751
mrhouzlane <mrhouzlane@github.com>
2022-05-20 17:36:20 -0400

    cl

--- contracts/contracts/APIExperience.sol
+++ contracts/contracts/APIExperience.sol
@@ -21,8 +21,6 @@ contract APIExperience is ChainlinkClient {
     // When TimeLeft == 0 emit this Event
     event ExperienceFinished(bytes32 firstName, bytes32 lastName, uint256 TimeLeft);
 
-
-
     /**
      * Network: Kovan
      * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
@@ -41,25 +39,16 @@ contract APIExperience is ChainlinkClient {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestEndOfExperience() public returns (bytes32 requestId) 
+    function requestEnd() public returns (bytes32 requestId) 
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
         // Set the URL to perform the GET request on
-        request.add("get", "https://demo.storj-ipfs.com/ipfs/Qmbphx6C8FE2Ko6qTUmCanQBVyuufCgrRSTWkKwvBqyUds");
+        request.add("get", "http://worldtimeapi.org/api/timezone/America/New_York");
         
         // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        request.add("path", "timezone.get.responses.default"); // add a nested path 
+    
+        request.add("path", "unixtime"); // add a nested path 
         
         // Multiply the result by 1000000000000000000 to remove decimals
         int timesAmount = 10**18;
@@ -68,16 +57,35 @@ contract APIExperience is ChainlinkClient {
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
     }
-    
-    /**
-     * Receive the response in the form of uint256
-     */ 
-    function fulfill(bytes32 _requestId, uint256 _timeLeft) public recordChainlinkFulfillment(_requestId)
+  
+
+    function requestEndOfExperience() public returns (bytes32 requestId) 
     {
-        timeLeft = _timeLeft;
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        request.add("timezone", "2021-11-02");
+        request.add("get", "12111");
+        request.add("responses", "FL");
+        request.add("default", "l"); 
+        return sendChainlinkRequestTo(oracle, request, fee);
     }
 
-    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+    /**
+     * Callback function
+     */
+    function fulfill(bytes32 _requestId, bytes32 _firstName, bytes32 _lastName, uint256 _timeLeft) public recordChainlinkFulfillment(_requestId)
+    {
+        firstName = _firstName;
+        lastName = _lastName;
+        timeLeft = _timeLeft; 
+        emit ExperienceFinished (firstName, lastName, timeLeft);
+    }
+
+    // function getUser(uint256 _index) external view returns (uint32, string memory, string memory, uint32, bool) {
+    //     (uint32 decodedId, string memory decodedFirstName, string memory decodedLastName, uint32 decodedTimeLeft, bool decodedIsExpired ) = 
+    //         abi.decode(users[_index], (uint32, string, string, uint32, bool)); 
+    //     return (decodedId, decodedFirstName, decodedLastName, decodedTimeLeft, decodedIsExpired);
+    // }
+    
 }
 
 

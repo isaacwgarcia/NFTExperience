1480a81d00fa936dca5a7da7641529cbdc7122f5
Isaac Garcia <isaacwi@gmail.com>
2022-05-20 23:40:53 -0400

    ipfs mint test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    INFURA_IPFS: process.env.INFURA_IPFS,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

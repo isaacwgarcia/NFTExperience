ed0dad4c36665ce08dc639afac72dbdf55a06915
mrhouzlane <mrhouzlane@github.com>
2022-05-20 23:04:43 -0400

    logic

--- contracts/contracts/APIExperience.sol
+++ contracts/contracts/APIExperience.sol
@@ -2,44 +2,51 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// API : http://worldtimeapi.org/pages/schema
 
-
-contract APIExperience is ChainlinkClient {
+contract NFTExp is ChainlinkClient, ERC721 {
     using Chainlink for Chainlink.Request;
 
-    uint256 public timeLeft; 
-    bytes32 public firstName;
-    bytes32 public lastName;
-
-
+    uint256 public realTime; 
+    uint256 public additionTime;
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    // When TimeLeft == 0 emit this Event
-    event ExperienceFinished(bytes32 firstName, bytes32 lastName, uint256 TimeLeft);
+    mapping(bytes32 => uint256) requestToTokenId; 
+    mapping(bytes32 => bytes32) requestToNewTime; 
+    mapping(bytes32 => address) requestToSender;
+    mapping(address => uint256) public counter; 
 
-    /**
-     * Network: Kovan
-     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
-     * Node)
-     * Job ID: d5270d1c311941d0b08bead21fea7747
-     * Fee: 0.1 LINK
-     */
-    constructor() {
+    uint256 public requestId; 
+    uint256 public newTime; 
+
+    Experiences[] public experiences; 
+
+    struct Experiences {
+        uint256 ExperienceId; 
+        uint256 timeSpent; 
+    }
+ 
+
+    constructor() ERC721("Experience", "EX") {
         setPublicChainlinkToken();
         oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
         jobId = "d5270d1c311941d0b08bead21fea7747";
         fee = 0.1 * 10 ** 18; // (Varies by network and job)
     }
+
+    bytes32 public x ;
     
     /**
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestEnd() public returns (bytes32 requestId) 
+    function requestAPI() public returns(bytes32)
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
@@ -55,37 +62,54 @@ contract APIExperience is ChainlinkClient {
         request.addInt("times", timesAmount);
         
         // Sends the request
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-  
-
-    function requestEndOfExperience() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        request.add("timezone", "2021-11-02");
-        request.add("get", "12111");
-        request.add("responses", "FL");
-        request.add("default", "l"); 
-        return sendChainlinkRequestTo(oracle, request, fee);
+        sendChainlinkRequestTo(oracle, request, fee);
+        return x; 
+        counter[msg.sender] = 0; 
+        counter[msg.sender] ++ ; 
     }
 
     /**
      * Callback function
      */
-    function fulfill(bytes32 _requestId, bytes32 _firstName, bytes32 _lastName, uint256 _timeLeft) public recordChainlinkFulfillment(_requestId)
+    function fulfill(address , bytes32 requestId, uint256 _realTime, uint256 newId) public recordChainlinkFulfillment(requestId)
     {
-        firstName = _firstName;
-        lastName = _lastName;
-        timeLeft = _timeLeft; 
-        emit ExperienceFinished (firstName, lastName, timeLeft);
+        realTime = _realTime;
+        for (uint256 i; i<2 ; i++){
+            if (counter[msg.sender]==0){
+                additionTime = 0;
+                _safeMint(msg.sender, newId);
+            }else{
+                additionTime = realTime; 
+            }
+
+        }
+
+        uint256 updateTime = realTime + additionTime; 
+        
+
+
+        //0 : updateTime = realTime + 0 // 1pm + nftMinted 
+        //1 : updateTime = realTime + realTime(additionTime) // 
+        
     }
 
-    // function getUser(uint256 _index) external view returns (uint32, string memory, string memory, uint32, bool) {
-    //     (uint32 decodedId, string memory decodedFirstName, string memory decodedLastName, uint32 decodedTimeLeft, bool decodedIsExpired ) = 
-    //         abi.decode(users[_index], (uint32, string, string, uint32, bool)); 
-    //     return (decodedId, decodedFirstName, decodedLastName, decodedTimeLeft, decodedIsExpired);
-    // }
-    
+    function endingExperience(uint256 newTime, address recipient, uint256 newId) public returns (bytes32) {
+        requestAPI();  
+        newTime = asciiToInteger(x) + realTime ; 
+    }
+
+    function asciiToInteger(bytes32 x) public pure returns (uint256) {
+    uint256 y;
+    for (uint256 i = 0; i < 32; i++) {
+        uint256 c = (uint256(x) >> (i * 8)) & 0xff;
+        if (48 <= c && c <= 57)
+            y += (c - 48) * 10 ** i;
+        else
+            break;
+    }
+    return y;
+}
+
 }
 
 

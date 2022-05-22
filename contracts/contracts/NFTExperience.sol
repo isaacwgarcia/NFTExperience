41ca1328bb3f5effcce031b2b168db53eb302b74
mrhouzlane <mrhouzlane@github.com>
2022-05-22 02:43:32 -0400

    DynamicTourista

--- contracts/contracts/NFTExperience.sol
+++ contracts/contracts/NFTExperience.sol
@@ -6,67 +6,72 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./Escrow.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTExperience is ChainlinkClient, ERC721URIStorage {
+contract APITourista is ChainlinkClient, ERC721URIStorage {
     using Chainlink for Chainlink.Request;
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
-    uint256 public realTime; 
-    address public owner;
+    uint256 public realTime;
 
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    struct UserExperience {
-        string uri ;
-        address tourist;
+    struct UserTourista {
+        string uri;
+        address streamer;
         uint256 startingTime;
-        uint256 endTime; 
+        uint256 endTime;
     }
 
-    
-    
-    mapping(bytes32 => string) public requestToURI ; 
-    mapping(bytes32 => address ) public requestToSender;
+    mapping(bytes32 => string) requestToURI;
+    mapping(bytes32 => address) requestToSender;
 
-
-    UserExperience[] public experiences;
+    UserTourista[] public Touristas;
 
     // When TimeLeft == 0 emit this Event
-    event ExperienceFinished(bytes32 firstName, bytes32 lastName, uint256 TimeLeft);
+    event TouristaFinished(
+        bytes32 firstName,
+        bytes32 lastName,
+        uint256 TimeLeft
+    );
 
     /**
      * Network: Kovan
-     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
+     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel
      * Node)
      * Job ID: d5270d1c311941d0b08bead21fea7747
      * Fee: 0.1 LINK
      */
-
-    address payable escrowAddress;
-    Escrow escrow; 
-
-    constructor(address payable _escrowAddress) ERC721("EXP", "EXP") {
-        escrowAddress = payable(_escrowAddress);
-        escrow = Escrow(_escrowAddress);
+    constructor() ERC721("Tourista", "SP") {
         setPublicChainlinkToken();
         oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
         jobId = "d5270d1c311941d0b08bead21fea7747";
-        fee = 0.1 * 10 ** 18; // (Varies by network and job)
-        owner = msg.sender; 
+        fee = 0.1 * 10**18; // (Varies by network and job)
     }
 
-    
-     uint256 public newId;
-     uint256 public counter;
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
 
+    /**
+     * Callback function
+     */
 
-    function mintNFT(address recipient, string memory tokenURI) onlyMinted
+    uint256 public newId;
+    uint256 public counter;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    modifier onlyMinted() {
+        require(counter == 2);
+        _;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
         public
+        onlyMinted
         returns (uint256)
     {
         _tokenIds.increment();
@@ -78,72 +83,62 @@ contract NFTExperience is ChainlinkClient, ERC721URIStorage {
         return newItemId;
     }
 
-    function requesting(string memory uri) public returns (bytes32)
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
-        request.add("get", "http://worldtimeapi.org/api/timezone/America/New_York");    
-        request.add("path", "unixtime"); // add a nested path 
-        int timesAmount = 10**18;
+    function requesting(string memory uri) public returns (bytes32) {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        request.add(
+            "get",
+            "http://worldtimeapi.org/api/timezone/America/New_York"
+        );
+        request.add("path", "unixtime"); // add a nested path
+        int256 timesAmount = 10**18;
         request.addInt("times", timesAmount);
-        
-        
+
         // Sends the request
         bytes32 requestId = sendChainlinkRequestTo(oracle, request, fee);
         requestToURI[requestId];
-        requestToSender[requestId] = address(this); 
+        requestToSender[requestId] = msg.sender;
         counter++;
         return requestId;
     }
 
-    function fulfill(bytes32 _requestId, uint256 _realTime) public recordChainlinkFulfillment(_requestId)
+    function fulfill(bytes32 _requestId, uint256 _realTime)
+        public
+        recordChainlinkFulfillment(_requestId)
     {
-        uint256 newId = experiences.length;
+        uint256 newId = Touristas.length;
         uint256 startingTime;
         uint256 endTime;
         string memory uri;
-        
-        if (counter==1){
-            realTime = 0; 
+
+        if (counter == 1) {
+            realTime = 0;
             uint256 startingTime = realTime;
-            experiences.push(
-                UserExperience(
+            Touristas.push(
+                UserTourista(
                     requestToURI[_requestId],
-                    owner,
+                    msg.sender,
                     startingTime,
                     endTime
                 )
             );
 
-        _safeMint(requestToSender[_requestId], newId);
-
-        }else{
+            _safeMint(requestToSender[_requestId], newId);
+        } else {
             realTime = _realTime;
             uint256 endTime = realTime;
-            experiences.push(
-                UserExperience(
-                    requestToURI[_requestId] ,
-                    owner,
+            Touristas.push(
+                UserTourista(
+                    requestToURI[_requestId],
+                    msg.sender,
                     startingTime,
                     endTime
-                ) 
+                )
             );
         }
-    
     }
-
-
-    modifier onlyMinted(){
-        require(counter == 2 ); 
-        _;
-    }
-    
 }
-
-
-
-
-
-
-
-  
